ea27dde08c134314e1c3dea74f994bab1a7e4d1c
BoRonin <yurisboronin@gmail.com>
2018-04-01 22:01:25 +0300

    Ready for redesign

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
+    clearCart()
     const calc = document.querySelector('.calc')
 
     const calcButton = document.querySelector('.firstPick')
@@ -8,7 +9,21 @@ document.addEventListener('DOMContentLoaded', function(){
     calcButton2.addEventListener('click', e => activateSecondPick(e))
 
     const toTrashButton = document.querySelector('.toTrash')
-    toTrashButton.addEventListener('click', () => console.table(allProds))
+    toTrashButton.addEventListener('click', () => {
+        allProds[0].name = `Профиль ${profileSelect.value}`
+        allProds[1].name = `Кронштейн ${kronshSelect.value}`
+        allProds[2].name = `Наконечник ${endSelect.value}`
+        allProds[3].name = `Кольца ${ringSelect.value}`
+        const typeCircle = document.querySelector('.statusLineStepThree')
+        typeCircle.classList.remove('statusLineStepNext')
+        typeCircle.classList.add('statusLineStepActive')
+        typeCircle.innerHTML = `&#10004; <span>Готово!</span>`
+        setTimeout(function(){
+            clearCart()
+            prod(allProds)
+        }, 300)
+
+    })
 
     const colorSelect = document.querySelector('.color');
     const lengthSelect = document.querySelector('.length');
@@ -16,30 +31,50 @@ document.addEventListener('DOMContentLoaded', function(){
     const kronshSelect = document.querySelector('.kronsh');
     const endSelect = document.querySelector('.end');
     const ringSelect = document.querySelector('.ring');
-    profileSelect.addEventListener('change', function() {
-        if (profileSelect.value.startsWith('440')) {
-            if (lengthSelect[0].value = 200) {
-                lengthSelect[0].value = 150
-                lengthSelect[0].innerHTML = 150
-                lengthSelect[1].value = 200
-                lengthSelect[1].innerHTML = 200
-                lengthSelect[2].value = 240
-                lengthSelect[2].innerHTML = 240
-            } return
-
+    lengthSelect.addEventListener('change', () => {
+        const temp = [...profileSelect[profileSelect.selectedIndex].value]
+        const finalTemp = `${temp[0]}${temp[1]}${temp[2]}${lengthSelect.value}${temp[6]}${temp[7]}${temp[8]}`
+        profileSelect[profileSelect.selectedIndex].value = finalTemp
+        profileSelect[profileSelect.selectedIndex].innerHTML = finalTemp
+        allProds[0].ts = Number(profileSelect.value)
+        if (lengthSelect.value == 240 || lengthSelect.value == 300) {
+            allProds[1].quantity = 3
+            allProds[1].amount = allProds[1].quantity*allProds[1].price
         } else {
+            allProds[1].quantity = 2
+            allProds[1].amount = allProds[1].quantity*allProds[1].price
+        }
+        adjustRingQuantity(lengthSelect)
+
+    })
+    profileSelect.addEventListener('change', () => {
+        if (profileSelect.value.startsWith('440')) {
             if (lengthSelect[0].value = 150) {
+                lengthSelect.selectedIndex = 0
                 lengthSelect[0].value = 200
                 lengthSelect[0].innerHTML = 200
                 lengthSelect[1].value = 240
                 lengthSelect[1].innerHTML = 240
                 lengthSelect[2].value = 300
                 lengthSelect[2].innerHTML = 300
-            } return
-        }
+                allProds[0].ts = profileSelect.value
+            }
 
+        } else {
+            if (lengthSelect[0].value = 200) {
+                lengthSelect.selectedIndex = 0
+                lengthSelect[0].value = 150
+                lengthSelect[0].innerHTML = 150
+                lengthSelect[1].value = 200
+                lengthSelect[1].innerHTML = 200
+                lengthSelect[2].value = 240
+                lengthSelect[2].innerHTML = 240
+            }
+        }
+        allProds[0].ts = Number(profileSelect.value)
+        adjustRingQuantity(lengthSelect)
     })
-    colorSelect.addEventListener('change', function(){
+    colorSelect.addEventListener('change', () => {
         const lastLetters = [...colorSelect.value]
 
         for(let i = 0; i < profileSelect.length; i++) {
@@ -87,17 +122,24 @@ document.addEventListener('DOMContentLoaded', function(){
         allProds[1].ts = Number(kronshSelect.value)
         allProds[2].ts = Number(endSelect.value)
         allProds[3].ts = Number(ringSelect.value)
+        if (allProds[4]) {
+            allProds[4].ts = Number(`435605${colorSelect.value}`)
+
+        }
 
     })
-    kronshSelect.addEventListener('change', function(){
-        adjustKrone(kronshSelect, colorSelect)
+    kronshSelect.addEventListener('change', () => {
+        adjustKrone(kronshSelect, colorSelect, lengthSelect)
     })
 })
 
-function adjustKrone(kronshSelect, colorSelect){
+function adjustKrone(kronshSelect, colorSelect, lengthSelect){
+    allProds[1].ts = Number(kronshSelect.value)
     const kronshValue = [...kronshSelect.value]
     const kronshType = Number(`${kronshValue[3]}${kronshValue[4]}${kronshValue[5]}`)
-    if (kronshType === 119 && !allProds[5]) {
+    if (kronshType === 119 && !allProds[4]) {
+        allProds[0].quantity = 2
+        allProds[0].amount = allProds[0].price*allProds[0].quantity
         allProds.push({
                 amount : 2,
                 img:"https://pp.userapi.com/c9266/v9266282/2fa/oy2a54KWUoQ.jpg",
@@ -108,12 +150,22 @@ function adjustKrone(kronshSelect, colorSelect){
             })
 
     } else if (allProds[4]) {
+        allProds[0].quantity = 1
+        allProds[0].amount = allProds[0].price*allProds[0].quantity
         allProds.splice(-1, 1)
     }
+    adjustRingQuantity(lengthSelect)
 }
 
 
-
+function adjustRingQuantity(lengthSelect){
+    allProds[3].quantity = lengthSelect.value/10*allProds[0].quantity
+    allProds[3].amount = allProds[3].quantity*allProds[3].price
+}
+function adjustQuantity(e, num){
+    e.quantity = num
+    e.amount = e.quantity*e.price
+}
 function activateFirstPick(e){
 
     const elem = e.target.closest('.carType');
@@ -218,11 +270,11 @@ const allProds = [{
         quantity:1,
         ts:440200105,
     }, {
-        amount : 1,
+        amount : 2,
         img:"https://pp.userapi.com/c830209/v830209609/2d598/XovK7FVpYCM.jpg",
         name:"Кронштейн",
         price:1,
-        quantity:1,
+        quantity:2,
         ts:435110105,
     }, {
         amount : 1,
@@ -232,11 +284,11 @@ const allProds = [{
         quantity:1,
         ts:435601105,
     }, {
-        amount : 1,
+        amount : 20,
         img:"https://pp.userapi.com/c623321/v623321725/8805/3Zo4UJu8-Mw.jpg",
         name:"Кольца",
         price:1,
-        quantity:1,
+        quantity:20,
         ts:435035105,
 }]
 function clearCart(){
@@ -247,6 +299,7 @@ function clearCart(){
     tcart.amount = 0;
     tcart.prodamount = 0;
     tcart.total = 0;
+    document.querySelector('.t706__carticon-counter').innerHTML = tcart.total
 }
 function prod(array){
     clearCart();
